cdc0be1bc76243608935321fbba3d0bf9ab26e7d
gzunigah <73375661+gzunigah@users.noreply.github.com>
2020-11-05 22:07:29 -0600

    Update libreta4.py

--- libreta4.py
+++ libreta4.py
@@ -1,13 +1,14 @@
 def numerosdivisibles(lista):
     for n in range(0,len(lista)):
         if lista[n]%2==0 or lista[n]%3==0 or lista[n]%5==0:
-            resultado = print (lista[n], "$")
+            resultado = print("$",lista[n], "\t", end=' ')
             n=n+1
         else:
-            resultado = print(lista[n])
+            resultado = print(lista[n], "\t", end=' ')
             n=n+1
     return resultado
             
+    
 
 numeros = list(range(1, 101))
 valores = numerosdivisibles(numeros)
